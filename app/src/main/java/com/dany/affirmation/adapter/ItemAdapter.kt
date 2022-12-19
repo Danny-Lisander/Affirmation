e6aebf948e4a73c31b5ddbac49886bf70296ee82
Danny <81091123+Danny-Lisander@users.noreply.github.com>
2022-12-19 13:30:55 +0600

    end

---
+++ app/src/main/java/com/dany/affirmation/adapter/ItemAdapter.kt
@@ -0,0 +1,38 @@
+package com.dany.affirmation.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.dany.affirmation.R
+import com.dany.affirmation.model.Affirmation
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(p0: ItemViewHolder, p1: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
---
+++ app/src/main/java/com/dany/affirmation/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.dany.affirmation.model
+
+data class Affirmation(val stringResourceId: Int)
